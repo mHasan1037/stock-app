d41c6cfbb3062da5efa12ad98725d589066edb3b
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-04-22 01:52:37 +0600

    hero section is completed

--- src/pages/exchange/Exchange.js
+++ src/pages/exchange/Exchange.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 
 const Exchange = () => {
+  const location = useLocation()
+
+  console.log(location.state.ticket)
+
   return (
     <div>Exchange</div>
   )
--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './home.scss'
 import heroImg from '../../images/hero-phone.png'
+import { useNavigate } from 'react-router'
 
 
 const HomePage = () => {
+  const [ticket, setTicket] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) =>{
+     e.preventDefault();
+     navigate('/exchange', {state: {ticket}})
+     setTicket('')
+  }
+
   return (
     <div className='homeContainer'>
         <section className='hero-section'>
@@ -11,10 +21,10 @@ const HomePage = () => {
                 <h1><span className='stockWord'>Stock</span> Exchange</h1>
                 <h3>Buy, sell, trade anytime</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <div className='heroSearch'>
-                    <button>Search</button>
-                    <input type='text' placeholder='Search Ticket...'/> 
-                </div>
+                <form className='heroSearch' onSubmit={handleSubmit}>
+                    <button type='submit'>Search</button>
+                    <input type='text' value={ticket} placeholder='Search Ticket...' onChange={(e)=> setTicket(e.target.value)} required/> 
+                </form>
             </aside>
             <div className='heroImg'>
               <img src={heroImg} />
